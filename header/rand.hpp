a3823acc506f96a171e08d240e5086865f310339
khamm004 <khamm004@ucr.edu>
2021-03-02 15:28:40 -0800

    Transferred derived classes from lab 3
    * Added destructors
    * Added functionality for new member function number_of_children

---
+++ header/rand.hpp
@@ -0,0 +1,17 @@
+#ifndef __RAND_HPP__
+#define __RAND_HPP__
+
+#include "base.hpp"
+
+class Rand : public Base {
+    private:
+	double value;
+    public:
+        Rand() { value = rand() % 100; }
+
+        virtual double evaluate() { return value; }
+        virtual std::string stringify() { return std::to_string(value); }
+	virtual int number_of_children(){ return 0;} 
+};
+
+#endif //__RAND_HPP__
